1db4f2d03597e77a42fca93e700fa0c270949490
darshanpadia <darshan.padia@somaiya.edu>
2025-06-29 11:54:32 +0530

    - Initialized Flask app using the application factory pattern - Created .flaskenv for environment-based auto-loading - Structured project using blueprint-centric modular layout:   • app/auth: authentication routes, services, forms, models   • app/records: student records features (CRUD)   • app/templates & app/static: for shared HTML and static files   • app/extensions.py: centralized initialization of db, csrf, etc. - Created initial folder layout with __init__.py files for import support - Prepared for future additions: Flask-Migrate (migrations/), and .env configuration

---
+++ app/__init__.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_wtf.csrf import CSRFProtect
+from config import Config
+
+db = SQLAlchemy()
+migrate = Migrate()
+csrf = CSRFProtect()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    migrate.init_app(app,db)
+    csrf.init_app()
+
+    return app
